243f6c760535f75f10134e2d62852719305fb88c
Johan Kämpe <kampe.johan@gmail.com>
2017-10-10 09:30:05 +0200

    Use onboard LED

--- School/laboration_001/lab01_leds/lab01_leds.ino
+++ School/laboration_001/lab01_leds/lab01_leds.ino
@@ -12,23 +12,24 @@ gång i sekunden.
 #define LED_PIN_RED PA1
 #define LED_PIN_GREEN PA2
 #define LED_PIN_BLUE PA3
+#define LED_ON_BOARD PC13
 
 /* Blink Red LED once per seconds */
-static void vLEDRedBlink(void *pvParameters)
+static void vLEDBlink(void *pvParameters)
 {
     while(1)
     {
         vTaskDelay(1000);
-        digitalWrite(LED_PIN_RED, HIGH);
+        digitalWrite(LED_ON_BOARD, HIGH);
         vTaskDelay(50);
-        digitalWrite(LED_PIN_RED, LOW);
+        digitalWrite(LED_ON_BOARD, LOW);
     }
 }
 void setup()
 {
-    pinMode(LED_PIN_RED, OUTPUT);
+    pinMode(LED_ON_BOARD, OUTPUT);
 
-    xTaskCreate(vLEDRedBlink,
+    xTaskCreate(vLEDBlink,
                 "REDBLINK",
                 configMINIMAL_STACK_SIZE,
                 NULL,
