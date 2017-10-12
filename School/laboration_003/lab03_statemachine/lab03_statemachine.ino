691909b8a3d3bd31bdb89fa6240db483e25f258c
Johan Kämpe <kampe.johan@gmail.com>
2017-10-12 11:12:19 +0200

    Fade two LEDS with state machine

--- School/laboration_003/lab03_statemachine/lab03_statemachine.ino
+++ School/laboration_003/lab03_statemachine/lab03_statemachine.ino
@@ -5,74 +5,73 @@
 #include <MapleFreeRTOS821.h>
 
 #define LED_PIN_BLUE PA1
+#define LED_PIN_RED PA2
 #define TICKER_MAX 255
 
 enum{UP, DOWN, TOP, BOTTOM};
+enum{RED, BLUE};
 
-int state;
-int pwm;
-int ticker;
+int state_led1, state_led2;
+int pwm_led1, pwm_led2;
+int ticker1, ticker2;
 
-void change_pwm(bool direction)
+void change_pwm(int * pwm, bool direction)
 {
     if(direction == UP)
     {
-        pwm++;
-        pwm = pwm > 255 ? 255 : pwm;
+        (*pwm)++;
+        *pwm = *pwm > 255 ? 255 : *pwm;
     }
 
     if(direction == DOWN)
     {
-        pwm--;
-        pwm = pwm < 0 ? 0 : pwm;
+        (*pwm)--;
+        *pwm = *pwm < 0 ? 0 : *pwm;
     }
 }
 
-void setup()
+void dim_led(int led)
 {
-    pinMode(LED_PIN_BLUE, OUTPUT);
-    state = UP;
-    pwm = 0;
-    analogWrite(LED_PIN_BLUE, pwm);
-}
+    int * state = led == RED ? &state_led1 : &state_led2;
+    int * pwm = led == RED ? &pwm_led1 : &pwm_led2;
+    int * ticker = led == RED ? &ticker1 : &ticker2;
+    int pin = led == RED ? LED_PIN_RED : LED_PIN_BLUE;
 
-void loop()
-{
-    switch(state)
+    switch(*state)
     {
         case UP:
-            change_pwm(UP);
-            analogWrite(LED_PIN_BLUE, pwm);
-            if(ticker == TICKER_MAX)
+            change_pwm(pwm, UP);
+            analogWrite(pin, *pwm);
+            if(*ticker == TICKER_MAX)
             {
-                ticker = 0;
-                state = TOP;
+                *ticker = 0;
+                *state = TOP;
             }
             break;
 
         case TOP:
-            if(ticker == TICKER_MAX)
+            if(*ticker == TICKER_MAX)
             {
-                ticker = 0;
-                state = DOWN;
+                *ticker = 0;
+                *state = DOWN;
             }
             break;
 
         case DOWN:
-            change_pwm(DOWN);
-            analogWrite(LED_PIN_BLUE, pwm);
-            if(ticker == TICKER_MAX)
+            change_pwm(pwm, DOWN);
+            analogWrite(pin, *pwm);
+            if(*ticker == TICKER_MAX)
             {
-                ticker = 0;
-                state = BOTTOM;
+                *ticker = 0;
+                *state = BOTTOM;
             }
             break;
 
         case BOTTOM:
-            if(ticker == TICKER_MAX)
+            if(*ticker == TICKER_MAX)
             {
-                ticker = 0;
-                state = UP;
+                *ticker = 0;
+                *state = UP;
             }
             break;
 
@@ -80,6 +79,24 @@ void loop()
             break;
     }
 
-    ticker++;
+    *ticker = *ticker + 1;
+}
+
+void setup()
+{
+    pinMode(LED_PIN_BLUE, OUTPUT);
+    pinMode(LED_PIN_RED, OUTPUT);
+    state_led1 = UP;
+    state_led2 = DOWN;
+    pwm_led1 = 0;
+    pwm_led2 = 255;
+    analogWrite(LED_PIN_BLUE, pwm_led1);
+    analogWrite(LED_PIN_RED, pwm_led2);
+}
+
+void loop()
+{
+    dim_led(RED);
+    dim_led(BLUE);
     delay(20);
 }
