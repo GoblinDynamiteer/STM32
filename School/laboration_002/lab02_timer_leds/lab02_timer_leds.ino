70b9d5ff76abd8f078d668f761fb91b51f4e7bcd
Johan Kämpe <kampe.johan@gmail.com>
2017-10-12 19:56:05 +0200

    Use button ISR to toggle LED dimming

--- School/laboration_002/lab02_timer_leds/lab02_timer_leds.ino
+++ School/laboration_002/lab02_timer_leds/lab02_timer_leds.ino
@@ -9,28 +9,41 @@ STEG 1. Haka en ISR på en knapp!
     lägg dock koden Klipp och klistra är tillåtet så länge som
     FreeRTOS _inte_ används.
 
+
+    2. Tryckknapp som triggar avbrott:
+    använd denna för att stänga av och sätta på dimmer-funktionen,
+    t.ex. så att när knappen trycks, så släcks LED:en och förblir avstängd,
+    när den trycks en gång till så börjar den dimma upp och ner igen.
 */
 
 #include <MapleFreeRTOS821.h>
 
 #define LED_PIN_RED PA0
+#define INT_PIN PA7 // Button connected with pulldown
 #define DIM_DELAY_RED 5
 #define PWM_MAX 255
 #define PWM_MIN 0
-#define TICKER_MAX 255;
+#define TICKER_MAX 255
+#define BUTTON_DELAY 200
 
 enum{UP, DOWN};
+enum{NO, YES};
+
+unsigned long button_timer;
+bool button_pressed;
 
 struct _led{
     int pin;
     int delay;
     bool direction;
     int pwm;
+    bool run;
 }led = {
     LED_PIN_RED,
     DIM_DELAY_RED,
     UP,
     0,
+    YES
 };
 
 void set_pwm(bool direction)
@@ -46,9 +59,27 @@ void set_pwm(bool direction)
     }
 }
 
+/* Toggle dimming of LED when button is pressed */
+void button_isr(void)
+{
+    if(!button_pressed && millis() - button_timer > BUTTON_DELAY)
+    {
+        button_pressed = YES;
+        led.run = !led.run;
+    }
+
+}
+
 void setup()
 {
     pinMode(led.pin, OUTPUT);
+
+    /* Interrupt pin / button pin */
+    pinMode(INT_PIN, INPUT_PULLUP);
+    attachInterrupt(INT_PIN, button_isr, FALLING);
+
+    button_timer = millis();
+    button_pressed = NO;
 }
 
 /* Dim LED up/down with main loop instead of FreeRTOS */
@@ -56,14 +87,24 @@ void loop()
 {
     while(1)
     {
-        set_pwm(led.direction);
-        analogWrite(led.pin, led.pwm);
+        if(led.run)
+        {
+            set_pwm(led.direction);
+            analogWrite(led.pin, led.pwm);
 
-        if( (led.pwm == PWM_MAX && led.direction == UP) ||
-            (led.pwm == PWM_MIN && led.direction == DOWN))
-             {
-                  led.direction = !led.direction;
-             }
+            /* Swap fade direction if max/min pwm reached */
+            if( (led.pwm == PWM_MAX && led.direction == UP) ||
+                (led.pwm == PWM_MIN && led.direction == DOWN))
+                {
+                    led.direction = !led.direction;
+                }
+        }
+
+        if(button_pressed && millis() - button_timer > BUTTON_DELAY)
+        {
+            button_pressed = NO;
+            button_timer = millis();
+        }
 
         delay(led.delay);
     }
