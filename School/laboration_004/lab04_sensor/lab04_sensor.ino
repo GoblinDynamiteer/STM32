b4bd89e7a888be2a2d3fe50d40bf190ec5e40d7e
Johan Kämpe <kampe.johan@gmail.com>
2017-10-16 14:56:12 +0200

    Use semaphors to block tasks

--- School/laboration_004/lab04_sensor/lab04_sensor.ino
+++ School/laboration_004/lab04_sensor/lab04_sensor.ino
@@ -1,11 +1,11 @@
 #include <MapleFreeRTOS900.h>
 
-#define COUNTER_PIN_1 PA0
-#define COUNTER_PIN_2 PA1
-#define COUNTER_PIN_4 PA2
-#define COUNTER_PIN_8 PA3
+#define COUNTER_CLICKER_PIN_AMOUNT 4
+#define COUNTER_CLICKER_TRESHOLD 6
 
-int counter_value;
+int counter_pins[COUNTER_CLICKER_PIN_AMOUNT] = {
+    PA0, PA1, PA2, PA3
+};
 
 struct freq {
     int LED;
@@ -16,13 +16,31 @@ struct freq {
     {PA9,   74,  7}
 };
 
-void get_counter_value(void)
+SemaphoreHandle_t xSemaphore = NULL;
+
+static void pollClickerCounter(void *pvParameters)
 {
-counter_value =
-        (!digitalRead(COUNTER_PIN_1) ? 1 : 0) +
-        (!digitalRead(COUNTER_PIN_2) ? 2 : 0) +
-        (!digitalRead(COUNTER_PIN_4) ? 4 : 0) +
-        (!digitalRead(COUNTER_PIN_8) ? 8 : 0);
+    static int counter_value = 0;
+
+    while(1)
+    {
+        if(xSemaphoreTake(xSemaphore, (TickType_t)100) == pdTRUE)
+        {
+            counter_value = 0;
+            while(counter_value < COUNTER_CLICKER_TRESHOLD)
+            {
+                for(int i = 0; i < COUNTER_CLICKER_PIN_AMOUNT; i++)
+                {
+                    counter_value |= (!digitalRead(counter_pins[i]) ?
+                        (1 << i) : 0);
+                }
+            }
+
+            xSemaphoreGive(xSemaphore);
+        }
+
+        else;
+    }
 }
 
 static void vLEDFlashTask(void *pvParameters)
@@ -32,12 +50,13 @@ static void vLEDFlashTask(void *pvParameters)
 
     for (;;)
     {
-        int ix;
-        vTaskDelay(Lfreq->longDelay);
-        get_counter_value();
-
-        if(counter_value > 5)
+        if(xSemaphoreTake(xSemaphore, (TickType_t)0) == pdTRUE)
         {
+            xSemaphoreGive(xSemaphore);
+
+            int ix;
+            vTaskDelay(Lfreq->longDelay);
+
             for (ix = 0; ix < 256; ix++)
             {
                 analogWrite(Lfreq->LED, ix);
@@ -57,12 +76,12 @@ static void vLEDFlashTask(void *pvParameters)
 
 void setup()
 {
-    pinMode(COUNTER_PIN_1, INPUT_PULLUP);
-    pinMode(COUNTER_PIN_2, INPUT_PULLUP);
-    pinMode(COUNTER_PIN_4, INPUT_PULLUP);
-    pinMode(COUNTER_PIN_8, INPUT_PULLUP);
+    for(int i = 0; i < COUNTER_CLICKER_PIN_AMOUNT; i++)
+    {
+        pinMode(counter_pins[i], INPUT_PULLUP);
+    }
 
-    counter_value = 0;
+    xSemaphore = xSemaphoreCreateMutex();
 
     xTaskCreate(vLEDFlashTask,
         "LEDblink1",
@@ -75,6 +94,13 @@ void setup()
         "LEDblink2",
         configMINIMAL_STACK_SIZE,
         (void *)&LEDfreq[1],
+        tskIDLE_PRIORITY + 2,
+        NULL);
+
+    xTaskCreate(pollClickerCounter,
+        "Poller",
+        configMINIMAL_STACK_SIZE,
+        NULL,
         tskIDLE_PRIORITY + 2,
         NULL);
 
